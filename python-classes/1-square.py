7457167a2df0821e3be2b6333805ff2f1d5b451e
KananKarim <kenankerimli7770@gmail.com>
2024-02-03 10:12:50 -0800

    private size

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Square class """
+
+
+class Square:
+    """ Square class, holding size """
+    def __init__(self, size):
+        """ Initialize a new size """
+        self.__size = size
