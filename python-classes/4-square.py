c145b60e235ec8fb88e372f61a35960dc9ca081b
KananKarim <kenankerimli7770@gmail.com>
2024-02-03 10:21:08 -0800

    Access and update private attribute

---
+++ python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Square class """
+
+
+class Square:
+    def __init__(self, size=0):
+        """ İnitializer method """
+        self.__size = size
+
+    @property
+    def size(self):
+        """ getter """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ setter """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ area """
+        return self.__size ** 2
