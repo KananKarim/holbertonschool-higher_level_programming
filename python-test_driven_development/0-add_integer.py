8e1e6b47133b61a9b6ce49dee8f325d637ec52cf
KananKarim <kenankerimli7770@gmail.com>
2024-02-04 16:22:19 +0400

    float overflow

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -15,6 +15,7 @@ if __name__ == "__main__":
     print(add_integer(1, 2))
     print(add_integer(100, -2))
     print(add_integer(2))
+    print(add_integer(float("nan")))
     print(add_integer(100.3, -2))
     try:
         print(add_integer(4, "School"))
