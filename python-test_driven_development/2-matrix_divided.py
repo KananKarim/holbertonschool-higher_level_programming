f1d2b24b7f0d9995b9398e8ba3ffd92a822413e5
KananKarim <kenankerimli7770@gmail.com>
2024-02-04 17:58:23 +0400

    remove space

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -11,7 +11,7 @@ def matrix_divided(matrix, div):
     if (not all(isinstance(i, list) and
                 all(isinstance(j, (int, float)) for j in i) for i in matrix)):
         raise TypeError("matrix must be a matrix\
-(list of lists) of integers/floats")
+ (list of lists) of integers/floats")
     if not all(len(i) == len(matrix[0]) for i in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     return [[round(j / div, 2) for j in i] for i in matrix]
@@ -19,7 +19,7 @@ def matrix_divided(matrix, div):
 
 if __name__ == "__main__":
     matrix = [
-        [1, 2, 3],
+        [1, 2, "3"],
         [4, 5, 6]
     ]
     print(matrix_divided(matrix, float("inf")))
