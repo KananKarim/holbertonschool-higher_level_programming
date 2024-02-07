b000c81ef400d3b816fa8ec6d3cfc85dea868c59
KananKarim <kenankerimli7770@gmail.com>
2024-02-07 17:29:07 +0400

    isSame class?

--- python-inheritance/test.py
+++ python-inheritance/test.py
@@ -1,11 +1,1 @@
-class MyList(list):
-    def print_sorted(self):
-        print(sorted(self))
-        
-
-l1 = MyList()
-l1.append(1)
-l1.append(2)
-l1.append(3)
-l1.append(4)
-l1.print_sorted()
+print(isinstance(1,object))
