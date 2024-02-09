a712c78b1e582de3835c2d8e51c6144dd1729937
KananKarim <kenankerimli7770@gmail.com>
2024-02-09 15:21:32 +0400

    str modified

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" doc """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square class """
+    def __init__(self, size):
+        """ init method """
+        super().integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """ str method """
+        return f"[Square] {self.__size}/{self.__size}"
