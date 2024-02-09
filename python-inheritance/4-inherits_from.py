ed1e14596ae205afb96f7d19b9d4a49a650f9449
KananKarim <kenankerimli7770@gmail.com>
2024-02-09 13:55:53 +0400

    check for inherit

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
-"""  """
+""" check inherit """
+
+
+def inherits_from(obj, a_class):
+    """ inherit function """
+    return isinstance(obj, a_class) and type(obj) is not a_class
