16e08f83d75469c85b08284e919690917a3cb955
KananKarim <kenankerimli7770@gmail.com>
2024-02-10 14:22:23 +0400

    new line removed

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """ read_file method """
 
-    with open(filename) as reader:
-        print(reader.read())
+    with open(filename, encoding="utf-8") as reader:
+        print(reader.read(), end="")
