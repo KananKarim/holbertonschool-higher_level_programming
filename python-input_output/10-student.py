f799a54fd43bdb9f5a02d5efc1cc5ad2d6e6d168
KananKarim <kenankerimli7770@gmail.com>
2024-02-10 17:11:48 +0400

    filter json

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" documentation """
+
+
+class Student:
+    """ Student class """
+    def __init__(self, first_name, last_name, age):
+        """ init method """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ to json """
+        if isinstance(attrs, list) and\
+                all(isinstance(it, str) for it in attrs):
+            new_dict = {}
+            for el in attrs:
+                if self.__dict__.get(el):
+                    new_dict[el] = self.__dict__[el]
+            return new_dict
+        return self.__dict__
