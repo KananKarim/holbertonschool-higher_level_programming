68715beb1d243e77083c74435eaf2b0d0ff0b279
KananKarim <kenankerimli7770@gmail.com>
2024-03-24 12:31:48 +0000

    cities added

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("SELECT cities.name FROM cities \
                  JOIN states ON cities.state_id = states.id \
-                 WHERE states.name = %s ORDER BY cities.id ASC", (sys.argv[4],))
+                 WHERE states.name = %s\
+                 ORDER BY cities.id ASC", (sys.argv[4],))
     rows = cur.fetchall()
     print(", ".join([row[0] for row in rows]))
     cur.close()
     db.close()
-
